33f569d78110d19d810df627e4d7db1f50614262
unknownZandbak <rjwillard01@gmail.com>
2022-08-24 20:46:22 +0200

    added  extra methods for getting & removing slaves

--- warehouse.py
+++ warehouse.py
@@ -3,16 +3,28 @@ from shelf import Shelf
 
 class Warehouse:
     
-    def __init__(self, employees:list[Employee]|None, shelves:list[Shelf]|None) -> None:
+    def __init__(self, employees :list[Employee] = [], shelves :list[Shelf] = []) -> None:
         self.employees = employees
         self.shelves = shelves
+
+    def getEmployees(self) -> list[Employee]:
+        return self.employees
+
+    def getShelves(self) -> list[Shelf]:
+        return self.shelves
 
     def addEmployee(self, employee:Employee) -> None:
         self.employees.append(employee)
 
+    def removeEmployee(self, employee:Employee) -> None:
+        self.employees.remove(employee)
+
     def addShelf(self, shelf:Shelf) -> None:
         self.employees.append(shelf)
     
+    def removeShelf(self, shelf:Shelf) -> None:
+        self.shelves.remove(shelf)
+
     def rearrangeShelf(self, shelf:Shelf) -> bool:
         pass
 
